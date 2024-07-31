c95f03f57732a0d5b0f4fb11c02b2db92d687a96
Ventura <ventura.py3@gmail.com>
2024-07-31 12:01:49 -0300

    first commit

---
+++ workshop-asp-net-core-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace workshop_asp_net_core_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
