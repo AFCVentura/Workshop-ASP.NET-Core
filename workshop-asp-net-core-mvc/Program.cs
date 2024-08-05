7fda9f728c1ae7414b50683e1492b85549244a68
Ventura <ventura.py3@gmail.com>
2024-08-05 00:24:26 -0300

    feat(Seeding Service): add the class SeedingService to populate the db with data and creat a scope to run the service if it's develop environment

--- workshop-asp-net-core-mvc/Program.cs
+++ workshop-asp-net-core-mvc/Program.cs
@@ -19,7 +19,12 @@ namespace workshop_asp_net_core_mvc
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<SeedingService>();
+
             var app = builder.Build();
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -28,7 +33,13 @@ namespace workshop_asp_net_core_mvc
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            else
+            {
+                // Set a seeding service
+                app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+            }
 
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
