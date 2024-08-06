0a696cfb65712832f79fe4986288b8d7ba9f0a07
Ventura <ventura.py3@gmail.com>
2024-08-06 10:11:48 -0300

    feat(Seller): Add SellersController and Index view

---
+++ workshop-asp-net-core-mvc/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace workshop_asp_net_core_mvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+    }
+}
